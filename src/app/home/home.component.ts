aa6bc91530e5363bd076773abb5d45ab1abe8e43
Wilson Manzano <wilson.manzanosanz@gmail.com>
2019-01-15 20:01:46 +0000

    New carousel with much better performance

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Flickity from 'flickity';
+//import Flickity from 'flickity';
 
 
 @Component({
@@ -9,18 +9,51 @@ import Flickity from 'flickity';
 })
 export class HomeComponent implements OnInit {
 
-  flkty: any;
-
+  //flkty: any;
+  public slideIndex = 1;
+  
   constructor() {
   }
 
   ngOnInit() {
-     this.flkty = new Flickity( '.carousel', {
+    this.showSlides(this.slideIndex);
+    this.timeout();
+    /* this.flkty = new Flickity( '.carousel', {
       // options
       wrapAround: true,
       contain: true,
       autoPlay: false,
-    });
+    });*/
+  }
+  
+  timeout() {
+    setTimeout(() => {
+        this.plusSlides(1);
+        this.timeout();
+    }, 6000);
+}
+  
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
 
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n) {
+    let i;
+    let slides:any = document.getElementsByClassName("mySlides");
+    let dots:any = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}    
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].style.display = "block";  
+    dots[this.slideIndex-1].className += " active";
   }
 }
